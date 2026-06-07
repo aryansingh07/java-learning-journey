714530cc7d41151c1de02f9b30003a30eacac534
Aryan Singh <53048215+aryansingh07@users.noreply.github.com>
2026-06-07 23:46:15 +0530

    Update ReverseArray.java
    Updated the reversedArray method to check for null input.

--- arrays/fundamentals/ReverseArray.java
+++ arrays/fundamentals/ReverseArray.java
@@ -2,7 +2,7 @@ package arrays.fundamentals;
 
 public class ReverseArray {
     public static int[] reversedArray(int[] arr, int left, int right) {
-        if (arr.length == 0)    throw new IllegalArgumentException("Please provide value for operating.");
+        if (arr == null || arr.length == 0)    throw new IllegalArgumentException("Please provide value for operating.");
 
 //        int left = 0;
 //        int right = arr.length - 1;
