a5d60fef7afcc608088f8d164680d57242c15bd0
Aryan Singh <53048215+aryansingh07@users.noreply.github.com>
2026-06-07 23:44:56 +0530

    Update LargestElement.java
    Added null check for input array in largestElement method.

--- arrays/fundamentals/LargestElement.java
+++ arrays/fundamentals/LargestElement.java
@@ -2,7 +2,7 @@ package arrays.fundamentals;
 
 public class LargestElement {
     public static int largestElement(int[] arr) {
-        if(arr.length == 0) throw new IllegalArgumentException("Please provide values for operation.");
+        if(arr == null || arr.length == 0) throw new IllegalArgumentException("Please provide values for operation.");
         int largest = 0;
 
         for (int j : arr) {
