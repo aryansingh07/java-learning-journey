2617a7796f8281d6b79eabc65cc3b4b87bdf7159
Aryan Singh <53048215+aryansingh07@users.noreply.github.com>
2026-06-07 23:43:52 +0530

    Update CheckSortedArray.java
    Updated the isArraySorted method to handle null arrays.

--- arrays/fundamentals/CheckSortedArray.java
+++ arrays/fundamentals/CheckSortedArray.java
@@ -2,7 +2,7 @@ package arrays.fundamentals;
 
 public class CheckSortedArray {
     public static boolean isArraySorted(int[] arr) {
-        if (arr.length == 0)    throw new IllegalArgumentException("Please provide values to operate.");
+        if (arr == null || arr.length == 0)    throw new IllegalArgumentException("Please provide values to operate.");
 
         int count = 0;
 
