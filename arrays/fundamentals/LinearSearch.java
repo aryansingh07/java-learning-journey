d9c0610d86b207c23c50725872d9779c4c937e6b
Aryan Singh <53048215+aryansingh07@users.noreply.github.com>
2026-06-07 23:45:17 +0530

    Update LinearSearch.java
    Updated linearSearch method to handle null arrays.

--- arrays/fundamentals/LinearSearch.java
+++ arrays/fundamentals/LinearSearch.java
@@ -2,7 +2,7 @@ package arrays.fundamentals;
 
 public class LinearSearch {
     public static int linearSearch(int[] arr, int x) {
-        if (arr.length == 0)    throw new IllegalArgumentException("Please provide values for operating.");
+        if (arr == null || arr.length == 0)    throw new IllegalArgumentException("Please provide values for operating.");
 
         for (int i = 0; i < arr.length; i++) {
             if ( arr[i] == x)   return i;
